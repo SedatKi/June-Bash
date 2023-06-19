34fb7a94d9944e445dbe480676935d59e2d6191e
Sedat <sedatkirgiz11@gmail.com>
2023-06-19 23:48:08 +0000

    class 1 files

---
+++ class_1/hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# echo "Hello World $$" ## $$ is to print out the PID
+
+ echo "$1 is currently eating $2, at the same time drinking $3 -- All arguements: $@"
+
+ echo "$0 script has input of $# arguements."
