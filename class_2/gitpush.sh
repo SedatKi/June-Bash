b4055f0a5c2ae5f149c500fe2ea88a7a9804da61
Sedat <sedatkirgiz11@gmail.com>
2023-06-20 01:58:04 +0000

    commiting class 2 work

---
+++ class_2/gitpush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d ".git" ] ; then
+    git status
+    git add $1
+    git commit -m "$2"
+    git push
+else
+    echo "You aren't in a repo"
+    read -p "Enter your repo name: " repo
+    repo_p=$(find ~/ -name June-Bash -type d)
+    if [ $repo_p != -z ]; then
+        cd $repo_p
+        ls -la
+    else
+        echo "repo $repo does not exist"
+    fi
+fi
+
