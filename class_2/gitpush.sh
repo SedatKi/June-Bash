2abd6824e6ffa372bb376ebb360131084b839296
Sedat <sedatkirgiz11@gmail.com>
2023-07-02 01:43:42 +0000

    Class 2 update

--- class_2/gitpush.sh
+++ class_2/gitpush.sh
@@ -11,9 +11,12 @@ else
     repo_p=$(find ~/ -name June-Bash -type d)
     if [ $repo_p != -z ]; then
         cd $repo_p
+        git status
+        git add $1
+        git commit -m "$2"
+        git push
         ls -la
     else
         echo "repo $repo does not exist"
     fi
 fi
-
